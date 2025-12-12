43eb53463a234e1d2481a1d02d2233255c870460
Bwimba Julien <bwimbajulien@gmail.com>
2025-12-12 15:00:16 +0200

    create a cron job

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from "express"
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsR from "./routes/transactions.js"
-
+import job from "../cron/cron.js";
 import dotenv from "dotenv"
 dotenv.config();
 
 const app = express();
+
+if (process.env.NOD_ENV === "production") job.start();
 
 //middleware
 app.use(rateLimiter);
@@ -16,6 +18,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 5001;
 
 
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+})
 app.use("/api/transactions", transactionsR)
 
 initDB().then(() => {
